87ec8a2203fd06c4f64be9e0f6d9c0c6afbda9df
EthanForrestCarr <ethan@ethancarr.com>
2024-11-08 22:57:35 -0600

    file structure and user routes

---
+++ server/src/models/User.ts
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    modelName: 'User',
+});
+
+export default User;
