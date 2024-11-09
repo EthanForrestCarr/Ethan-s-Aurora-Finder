b551ed3eb45cc119d690da706215ea7926309268
EthanForrestCarr <ethan@ethancarr.com>
2024-11-09 00:08:29 -0600

    HomePage and adjusted app.tsx

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LocationInput from '../components/LocationInput';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Aurora Finder</h1>
+            <LocationInput />
+        </div>
+    );
+};
+
+export default HomePage;
