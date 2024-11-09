89738aa9028fd8bca71015861d30a7f0e3138ec2
EthanForrestCarr <ethan@ethancarr.com>
2024-11-09 16:03:22 -0600

    fixed jwt type

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -13,7 +13,7 @@ const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunc
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY!);
         req.user = decoded;
         next();
     } catch (error) {
